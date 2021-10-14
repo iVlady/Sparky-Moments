bf66e63346a1c8bc8942f320b592d74aa38c9760
iVlady <vinogrotskiy97@gmail.com>
2021-10-14 22:53:27 +0300

    added logIn storyboard

--- Sparky Moments/View Controllers/SignInViewController.swift
+++ Sparky Moments/View Controllers/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SignUpViewController.swift
+//  SignInViewController.swift
 //  Sparky Moments
 //
 //  Created by Vlad Vinogrotskiy on 14.10.2021.
@@ -7,34 +7,24 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignInViewController: UIViewController {
 
-    @IBOutlet weak var fullNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var signUpButton: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        fullNameTextField.layer.borderWidth = 2
-        fullNameTextField.layer.borderColor = UIColor.lightGray.cgColor
         
         emailTextField.layer.borderWidth = 2
         emailTextField.layer.borderColor = UIColor.lightGray.cgColor
         
         passwordTextField.layer.borderWidth = 2
         passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
-        
-        signUpButton.layer.cornerRadius = 16
 
-    }
-    
-
-    @IBAction func signUpButtonPressed(_ sender: UIButton) {
         
     }
     
+
+    
+
 }
