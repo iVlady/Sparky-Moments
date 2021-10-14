c62a2f3b9b03263b8069bcb06c1a4edf428b26ec
iVlady <vinogrotskiy97@gmail.com>
2021-10-14 19:04:45 +0300

    Initial Commit

---
+++ Sparky Moments/View Controllers/StartPageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LogInOrSignUpViewController.swift
+//  Sparky Moments
+//
+//  Created by Vlad Vinogrotskiy on 14.10.2021.
+//
+
+import UIKit
+
+class StartPageViewController: UIViewController {
+
+    @IBOutlet weak var signIn: UIButton!
+    @IBOutlet weak var signUp: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        signIn.layer.cornerRadius = 10
+        
+    }
+    
+
+
+}
---
+++ Sparky Moments/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sparky Moments
+//
+//  Created by Vlad Vinogrotskiy on 13.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
