392a1a199daf2fdcf3ce683bc852dbc4d7852672
iVlady <vinogrotskiy97@gmail.com>
2021-10-15 12:27:47 +0300

    try constraints

--- Sparky Moments/View Controllers/SignInViewController.swift
+++ Sparky Moments/View Controllers/SignInViewController.swift
@@ -11,19 +11,30 @@ class SignInViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var underView: UIView!
+    @IBOutlet weak var signInButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        underView.layer.cornerRadius = 20
+        signInButton.layer.cornerRadius = 16
         
         emailTextField.layer.borderWidth = 2
         emailTextField.layer.borderColor = UIColor.lightGray.cgColor
         emailTextField.setLeftView(image: UIImage.init(systemName: "envelope")!, width: 30, height: 20)
         emailTextField.tintColor = .lightGray
+        emailTextField.clipsToBounds = true
+        emailTextField.layer.cornerRadius = 18
         
         passwordTextField.layer.borderWidth = 2
         passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
         passwordTextField.setLeftView(image: UIImage.init(systemName: "key")!, width: 20, height: 30)
         passwordTextField.tintColor = .lightGray
+        passwordTextField.clipsToBounds = true
+        passwordTextField.layer.cornerRadius = 18
+        
+        
 
     }
     
